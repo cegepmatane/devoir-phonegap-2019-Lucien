454e7789d0e194315f3cbb00f911c8390aeb50e0
Lucien Barcon <lucienbarcon@gmail.com>
2019-10-24 16:54:09 -0400

    Ajouter / Lister / Modifier en local storage

--- donnee/JeuDAO.js
+++ donnee/JeuDAO.js
@@ -1,5 +1,6 @@
 var JeuDAO = function () {
 
+    /*
     var listeJeu = [
         new Jeu("League Of Legend","Riot Games","5vs5 dans la faille de l'invocateur",0),
         new Jeu("Overwatch","Blizzard","Fps en 6vs6 inspiré de l'univers Blizzard",1),
@@ -8,16 +9,57 @@ var JeuDAO = function () {
 
     this.lister = function () {
         return listeJeu;
-    }
+    };
 
     this.ajouter = function (jeu) {
         jeu.id = listeJeu.length;
         listeJeu.push(jeu);
-    }
+    };
 
-    this.modifier = function (jeu) {
-        listeJeu[jeu.id].nom = jeu.nom;
-        listeJeu[jeu.id].description = jeu.description;
-        listeJeu[jeu.id].developpeur = jeu.developpeur;
-    }
+     */
+
+    var listeJeu = null;
+    var initialiser = function () {
+        if (!listeJeu) {
+            listeJeu = [];
+        }
+    };
+
+    this.lister = function() {
+      if (localStorage['jeu']) {
+          listeJeu = JSON.parse(localStorage['jeu']);
+      }
+
+      for (position in listeJeu) {
+          var jeu = new Jeu(listeJeu[position].nom,
+                            listeJeu[position].developpeur,
+                            listeJeu[position].developpeur,
+                            listeJeu[position].id);
+          listeJeu[position] = jeu;
+      }
+      return listeJeu;
+    };
+
+    this.ajouter = function(jeu) {
+
+        if (listeJeu.length > 0) {
+            console.log("epepepep")
+            jeu.id = listeJeu[listeJeu.length - 1].id + 1;
+            console.log(jeu.id);
+        }
+        else {
+            jeu.id = 0;
+        }
+
+        listeJeu[jeu.id] = jeu;
+        localStorage['jeu'] = JSON.stringify(listeJeu);
+    };
+
+    this.modifier = function(jeu) {
+
+        listeJeu[jeu.id] = jeu;
+        localStorage['jeu'] = JSON.stringify(listeJeu);
+    };
+
+    initialiser();
 };
